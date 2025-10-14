0d7c97244c713edd6a4ffedaaa500a7f5bc0569c
RiquelSantos <riquesantosoliveira@gmail.com>
2025-10-14 15:07:51 -0300

    feat: Adiciona resolução do ReverseString

---
+++ src/main/java/dev/henrique/ReverseString.java
@@ -0,0 +1,24 @@
+package dev.henrique;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        System.out.println(reverseString(new char[]{'h','e','l','l','o'}));
+    }
+
+    public static char[] reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left <= right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return s;
+    }
+
+}
